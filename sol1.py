345ffdde564afb8df562aa1c7de2ec8a8a35d453
Elias Ailenei <elias@elias.com>
2025-02-16 15:07:42 +0000

    fixed code, now it will give the correct output

--- sol1.py
+++ sol1.py
@@ -1,89 +1,44 @@
 def check(binary):
-    approve = True
-    for index in binary:
-        if index != "1":
-            if index != "0":
-                approve = False
-    return approve
-
-
-def convert(binary):
-    data = []
-    exponential = len(binary) - 1
-    for x in range(len(binary)):
-        data.append(2 ** exponential * int(binary[x]))
-        exponential -= 1
-    return data
-
-
+    return all(index in "01" for index in binary)
 def hidden(binary):
-    increment = 0
-    data = []
+    data = set()
+    length = len(binary)
     if check(binary):
-        while increment != len(binary):
-            string = ""
-            for x in range((len(binary)) - increment):
-                string = string + binary[x]
-            data.append(string)
-            increment += 1
-        return data
-
-
-def prime(array):
-    primed = [2, 3, 5, 7, 11]
-    for index in range(len(array)):
-        for element in primed:
-            if array[index] != element:
-                if array[index] > 3:
-                    if array[index] % element == 0:
-                        array[index] = -1
-
-                else:
-                    if array[index] == 1 or array[index] == 0:
-                        array[index] = -1
-
-    return array
-
-
+        prefix = [0] * (length + 1)  
+        for i in range(length):
+            prefix[i + 1] = (prefix[i] << 1) + int(binary[i])
+        for i in range(length):
+            for j in range(i + 1, length + 1):
+                val = prefix[j] - (prefix[i] << (j - i))
+                if val > 1:  
+                    data.add(val)
+    return sorted(data)  
+def is_prime(n):
+    if n < 2:
+        return False
+    if n in {2, 3, 5, 7, 11}:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 def sort(array, less):
-    array = prime(array)
-    swap = True
-    while swap:
-        swap = False
-        if array[0] == -1:
-            array.remove(-1)
-            swap = True
-        if array[-1] > less:
-            array[-1] = -1
-            swap = True
-
-        for element in range(len(array)):
-            for index in range(len(array) - 1):
-                if array[index] > array[index + 1]:
-                    temp = array[index]
-                    array[index] = array[index + 1]
-                    array[index + 1] = temp
-                    swap = True
-    return array
-
-
-def final(array, less):
-    array = sort(array, less)
+    array = [num for num in array if num <= less and is_prime(num)]
+    array = sorted(set(array))  
+    if len(array) == 0:
+        return "No primes found."
     if len(array) > 6:
-        print(array[0], array[1], array[2], array[-3], array[-2], array[-1], ":", len(array))
-    else:
-        print(array)
-
-
+        return f"{array[0]} {array[1]} {array[2]} {array[-3]} {array[-2]} {array[-1]} : {len(array)}"
+    return " ".join(map(str, array))
+def final(array, less):
+    print(sort(array, less))
 def work(array, less):
     values = hidden(array)
-    datas = []
-    for element1 in values:
-        total = 0
-        for element2 in convert(element1):
-            total += element2
-        datas.append(total)
-    final(datas, less)
-
-
-work("011011", 10)
+    final(values, less)
+test_binary = "01000011010011110100110101010000001100010011100000110001"
+test_limit = 123456789012
+work(test_binary, test_limit)
